faec57c6f71117664ac5ef58195e88420b8a6bc3
VictorEtec14 <victor.nascimento92@etec.sp.gov.br>
2024-09-18 14:25:38 -0300

    upload atividade

---
+++ AulaMVVMHTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,34 @@
+﻿using AulaMVVMHTTPClient.Models;
+using AulaMVVMHTTPClient.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AulaMVVMHTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Post> posts;
+        public PostService postsService;
+        public ICommand getPostsCommand { get; }
+
+
+        public PostViewModel() {
+            PostService postsService = new PostService();
+            getPostsCommand = new Command(getPosts);
+        }
+
+        public async void getPosts()
+        {
+            //Buscar os dados da API
+            PostService postService = new PostService();
+            posts = await postsService.GetPosts();
+        }
+    }
+}
